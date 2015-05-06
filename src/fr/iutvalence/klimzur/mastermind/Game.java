27a74cc8a17a69c5fca0944553c82e751b4645a8
klimczam <mathis.klimczak@iut-valence.fr>
2015-05-06 12:03:55 +0200

    End of class Game

--- src/fr/iutvalence/klimzur/mastermind/Game.java
+++ src/fr/iutvalence/klimzur/mastermind/Game.java
@@ -1,93 +1,102 @@
 package fr.iutvalence.klimzur.mastermind;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Scanner;
 
 public class Game {
-    /**
-     * The board displays
-     */
-    private Board  board;
-    /**
-     * The player who plays to this game
-     */
-    private Player player1;
-    
-    /**
-     * The round of the game
-     */
-    public static int round = 0;
-    
-    /**
-     * Table of color which contents the secret 
-     */
-    private final Color[] secret;
-    
-    public Color[] proposition;
-    
-    /**
-     * The constructor of the game
-     */
-    public Game(){
-    	this.player1 = new Player("JoueurTest");
-    	this.board = new Board();
-    	this.secret= new Color[4];
-    	this.secret[0]=Color.YELLOW;
-    	this.secret[1]=Color.ORANGE;
-    	this.secret[2]=Color.GREEN;
-    	this.secret[3]=Color.BLUE;
-    	proposition = new Color[4];
-    	this.proposition[0]=Color.ORANGE;
-    	this.proposition[1]=Color.BLUE;
-    	this.proposition[2]=Color.GREEN;
-    	this.proposition[3]=Color.RED;
-    }
-    
 
-    
-    private String verification(){
-    	int NombreDeCouleurBienPlacee = 0;
-    	int BonneCouleur = 0;
-    	for (int CouleurSecrete = 0; CouleurSecrete<4; CouleurSecrete++){
-    		for(int CouleurJouee = 0; CouleurJouee<4; CouleurJouee++){
-    			if (this.secret[CouleurSecrete]==this.proposition[CouleurJouee] && CouleurJouee == CouleurSecrete){
-    				NombreDeCouleurBienPlacee++;
-    			}
-    			if (this.secret[CouleurSecrete]==this.proposition[CouleurJouee] && CouleurJouee != CouleurSecrete){
-    				BonneCouleur++;
-    			}    			
-    		}
-    	}
-    	return "Nombre De Couleur Bien Placée : " + NombreDeCouleurBienPlacee + " | Couleur correcte, mauvais placement : " + BonneCouleur;
-    }
-    /**
-     * To get the secret
-     * @return the secret
-     */
-    public Color[] getSecret(){
-    	return this.secret;
-    }
+	private final int NUMBER_OF_ROUND = 8;
+	/**
+	 * The board displays
+	 */
+	private Board board;
+	/**
+	 * The player who plays to this game
+	 */
+	private Player player1;
 
+	/**
+	 * The round of the game
+	 */
+	public static int round = 0;
 
+	/**
+	 * Table of color which contents the secret
+	 */
+	private final Color[] secret;
+
+	/**
+	 * The constructor of the game
+	 */
+	public Game(String nickname) {
+		this.player1 = new Player(nickname);
+		this.board = new Board();
+		this.secret = new Color[] { Color.YELLOW, Color.ORANGE, Color.GREEN,
+				Color.BLUE };
+	}
 
 	public void play() {
-		System.out.println(this.board);
 		Scanner sc = new Scanner(System.in);
-		
-		for (int i = 0; i < proposition.length; i++) {
-			System.out.println("Entrez votre couleur: ");
-			String str = sc.nextLine();
-			if ( str.equals("BLUE")){
-				this.proposition[i]=Color.BLUE;
-			}
-			this.board.setCases(round, i, this.proposition[i]);
-		}
-		
+
+		System.out.println("Joueur : " + player1.toString());
 		System.out.println(this.board);
-		/*while(true){
-			// If gagner, break;
-		}*/
+
 		
+		boolean win = false;
+		for (int round = 0; round < NUMBER_OF_ROUND; round++) {
+			System.out.println("Tour : " + (round + 1));
+			
+			Color[] guess = new Color[4];
+			for (int i = 0; i < guess.length; i++) {
+				System.out.println("Entrez votre couleur: ");
+				String str = sc.nextLine();
+				// TODO try
+				guess[i] = Color.valueOf(str);
+				this.board.setCases(round, i, guess[i]);
+			}
+			
+			System.out.println(this.board);
+			String victory = verification(guess);
+
+			System.out.println(victory);
+			if (victory.equals("Gagné !")) {
+				win = true;
+				break;
+			}
+		}
+		if (win) {
+			// TODO Texte de victoire
+		} else {
+			System.out.println("Perdu ! La bonne combinaison est :"
+					+ Arrays.toString(secret));
+		}
+
+	}
+	
+	private String verification(Color[] proposition) {
+		int NombreDeCouleurBienPlacee = 0;
+		int BonneCouleur = 0;
+		for (int CouleurSecrete = 0; CouleurSecrete < 4; CouleurSecrete++) {
+			for (int CouleurJouee = 0; CouleurJouee < 4; CouleurJouee++) {
+				if (this.secret[CouleurSecrete] == proposition[CouleurJouee]
+						&& CouleurJouee == CouleurSecrete) {
+					NombreDeCouleurBienPlacee++;
+				}
+				if (this.secret[CouleurSecrete] == proposition[CouleurJouee]
+						&& CouleurJouee != CouleurSecrete) {
+					BonneCouleur++;
+				}
+			}
+		}
+		if (NombreDeCouleurBienPlacee == 4)
+			return "Gagné !";
+		else {
+			return "Nombre De Couleur Bien Placée : "
+					+ NombreDeCouleurBienPlacee
+					+ " | Couleur correcte, mauvais placement : "
+					+ BonneCouleur;
+		}
 	}
 
 }
