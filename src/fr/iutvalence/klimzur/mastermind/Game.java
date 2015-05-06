fae1ad763208483df524e8baa446f710bba0d26c
Théo <Tho@130.190.62.41>
2015-05-06 09:24:39 +0200

    Update Game

--- src/fr/iutvalence/klimzur/mastermind/Game.java
+++ src/fr/iutvalence/klimzur/mastermind/Game.java
@@ -1,10 +1,5 @@
 package fr.iutvalence.klimzur.mastermind;
 
-/**
- * The begin of the game
- * @author klimczam
- *
- */
 public class Game extends Board {
     /**
      * The board displays
@@ -18,33 +13,70 @@ public class Game extends Board {
     /**
      * The round of the game
      */
-    private int round;
+    public static int round = 0;
     
     /**
      * Table of color which contents the secret 
      */
     private final Color[] secret;
     
+    public Color[] proposition;
+    
     /**
      * The constructor of the game
      */
     public Game(){
     	this.player1 = new Player("JoueurTest");
-    	this.round = 0;
     	this.board = new Board();
     	this.secret= new Color[4];
     	this.secret[0]=Color.YELLOW;
     	this.secret[1]=Color.ORANGE;
     	this.secret[2]=Color.GREEN;
     	this.secret[3]=Color.BLUE;
+    	proposition = new Color[4];
+    	this.proposition[0]=Color.ORANGE;
+    	this.proposition[1]=Color.BLUE;
+    	this.proposition[2]=Color.GREEN;
+    	this.proposition[3]=Color.RED;
     }
     
+
+    
+    private String verification(){
+    	int NombreDeCouleurBienPlacee = 0;
+    	int BonneCouleur = 0;
+    	for (int CouleurSecrete = 0; CouleurSecrete<4; CouleurSecrete++){
+    		for(int CouleurJouee = 0; CouleurJouee<4; CouleurJouee++){
+    			if (this.secret[CouleurSecrete]==this.proposition[CouleurJouee] && CouleurJouee == CouleurSecrete){
+    				NombreDeCouleurBienPlacee++;
+    			}
+    			if (this.secret[CouleurSecrete]==this.proposition[CouleurJouee] && CouleurJouee != CouleurSecrete){
+    				BonneCouleur++;
+    			}    			
+    		}
+    	}
+    	return "Nombre De Couleur Bien Placée : " + NombreDeCouleurBienPlacee + " | Couleur correcte, mauvais placement : " + BonneCouleur;
+    }
     /**
      * To get the secret
      * @return the secret
      */
-    public Color[] GetSecret(){
+    public Color[] getSecret(){
     	return this.secret;
+    }
+    
+    public Color[][] PlacementCouleur(){
+    	for (int i = 0; i < NOMBRE_DE_LIGNES; i++) {
+            for (int j = 0; j < NOMBRE_DE_COLONNES; j++) {
+                this.cases[i][j] = Color.BLACK;
+            }         
+        }
+    	for (int k=0; k<round+1; k++){
+    		for (int e=0; e < NOMBRE_DE_COLONNES; e++){
+    			this.cases[k][e]=this.proposition[e];
+    		}
+        }
+    	return this.cases;
     }
     
     /**
@@ -53,16 +85,17 @@ public class Game extends Board {
     @Override
     public String toString() {
     	String plateauAsciiArt = "";
-    	plateauAsciiArt += "Round : " + this.round + "\n";
+    	plateauAsciiArt += "Round : " + round + "\n";
     	plateauAsciiArt += "Nom du joueur : " + this.player1.name + "\n";
-    	
+    	PlacementCouleur();
         for (int numeroDeLigne = 0; numeroDeLigne < NOMBRE_DE_LIGNES; numeroDeLigne++) {
             for (int numeroDeColonne = 0; numeroDeColonne < NOMBRE_DE_COLONNES; numeroDeColonne++) {
                 plateauAsciiArt += this.cases[numeroDeLigne][numeroDeColonne] + " ";
             }
             plateauAsciiArt += "\n";
         }
+        plateauAsciiArt += verification();
         return plateauAsciiArt;
     }
-    
+
 }
